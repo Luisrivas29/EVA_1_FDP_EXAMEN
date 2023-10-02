d301c59348568df896741aefc7479afd8bb49523
Tashiro13 <143568865+Tashiro13@users.noreply.github.com>
2023-10-01 22:14:36 -0600

    Add files via upload

---
+++ Examen_1/src/main/java/com/mycompany/examen_1/Examen_1.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mago1
+ */
+public class Examen_1 {
+
+    public static void main(String[] args) {
+   
+        String equipo;
+        String proye;
+        String Inte;
+        String InTe;
+        String inte;
+        
+        Scanner captu=  new Scanner(System.in);
+        
+        equipo = "Homeless Engineers";
+        proye = "Hotel Booking";
+        Inte = "Alexis Cruz Martinez 23550373";
+        InTe = "Daniel Isaias Navarrete Lucero 23550007";
+        inte = "Luis Raul Rivas Sanchez 23550331";
+        System.out.println("Nombre del equipo:");
+        System.out.println(equipo);
+        System.out.println("Nombre del proyecto:");
+        System.out.println(proye);
+        System.out.println("Integrantes:");
+        System.out.println(Inte);
+        System.out.println(InTe);
+        System.out.println(inte);
+
+        
+             
+        
+       
+        
+    }
+}
